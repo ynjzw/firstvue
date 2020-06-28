dca3fe0075d597f4940875806a2ece2f420b2f1f
ynjzw <55905855+ynjzw@users.noreply.github.com>
2020-06-28 17:28:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import test from "./components/test";
-import ElementUI from "element-ui"
-import ViewUI from "view-design"
-import 'view-design/dist/styles/iview.css'
 
-Vue.use(ViewUI)
+import test from "./components/test"
+import axios from 'axios'
+import router from './router'
+import ViewUI from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(ViewUI);
+Vue.prototype.$http=axios
 
 Vue.config.productionTip = false
 
